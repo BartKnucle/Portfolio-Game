580d37ed87678a89350000129f2655e0876a9669
BartKnucle <sjarde@yahoo.fr>
2021-04-01 15:59:38 +0200

    Added game service

--- Assets/Game/GameState.cs
+++ Assets/Game/GameState.cs
@@ -21,4 +21,8 @@ public class GameState : GenericSingletonClass<MonoBehaviour>
                 break;
         }
     }
+
+    public void onGameCreated() {
+      SceneManager.LoadScene("Game");
+    }
 }
